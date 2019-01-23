523bb9058b006f13ea9a1725833e1706ce1e1789
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-01-23 21:09:56 +0100

    Add popup with styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 /* Here goes your JS code */
+const button = document.querySelector('.btn');
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.closeBtn');
+
+function toggleClass() {
+    popup.classList.toggle('show');
+}
+
+button.addEventListener('click', toggleClass);
+closeBtn.addEventListener('click', toggleClass);
