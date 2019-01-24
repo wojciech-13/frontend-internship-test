aed62d73edf8311041991a1e409cff24affbea13
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-01-24 23:32:47 +0100

    added validation to form, hover effect for submit button and changed checkbox style

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,31 @@
 const button = document.querySelector('.btn');
 const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.closeBtn');
+const form = document.querySelector('.form');
 
+console.log(form);
+console.log(form.email);
 function toggleClass() {
     popup.classList.toggle('show');
 }
 
+function validateForm(e) {
+    if(this.email.value === ""){
+        e.preventDefault();
+        alert('E-mail field can not be empty');
+    }
+    if(this.password.value === ""){          //?
+        e.preventDefault();
+        alert('Password field can not be empty');
+    }
+    if(!this.checkbox.checked){
+        e.preventDefault();
+        alert('You must agree to the terms and conditions');
+    }
+}
+
 button.addEventListener('click', toggleClass);
 closeBtn.addEventListener('click', toggleClass);
+
+// Form Validation
+form.addEventListener('submit', validateForm);
